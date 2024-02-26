f5dd5eae9fa41bbd6a27640d2cd6ba5b705296d5
Joel Filho <joelfilho02812@gmail.com>
2024-02-26 14:24:36 -0300

    feat: add get logged user

--- backend/src/presentations/controllers/user-controller.ts
+++ backend/src/presentations/controllers/user-controller.ts
@@ -4,6 +4,11 @@ import UserService from "../../application/services/user-service";
 export default class UserController {
   constructor(private readonly _userService: UserService) {}
 
+  async getLoggedUser(request: FastifyRequest, reply: FastifyReply) {
+    const user = await this._userService.getLoggedUser(request);
+    return reply.status(200).send(user);
+  }
+
   async create(request: FastifyRequest, reply: FastifyReply) {
     await this._userService.create(request);
     return reply.status(201).send();
