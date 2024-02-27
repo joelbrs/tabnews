1ca973d5bffa9387a551ff2123be01a3c386d501
Joel Filho <joelfilho02812@gmail.com>
2024-02-27 15:47:00 -0300

    feat: add dayjs & initial configs

---
+++ frontend/src/lib/dayjs.ts
@@ -0,0 +1,26 @@
+import dayjs from 'dayjs'
+import UpdateLocale from 'dayjs/plugin/updateLocale'
+import RelativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(RelativeTime)
+dayjs.extend(UpdateLocale)
+
+dayjs.updateLocale('en', {
+  relativeTime: {
+    future: 'em %s',
+    past: '%s atrás',
+    s: 'alguns seconds',
+    m: 'um minute',
+    mm: '%d minutos',
+    h: 'uma hora',
+    hh: '%d horas',
+    d: 'um dia',
+    dd: '%d dias',
+    M: 'um mês',
+    MM: '%d meses',
+    y: 'um ano',
+    yy: '%d anos'
+  }
+})
+
+export const $dayjs = dayjs
