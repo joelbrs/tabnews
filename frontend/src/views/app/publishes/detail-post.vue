2f4150e32b36d0d5513f4a7fe00580c6983f2da2
Joel Filho <joelfilho02812@gmail.com>
2024-03-01 11:00:17 -0300

    fix: alignments in detail-post view

--- frontend/src/views/app/publishes/detail-post.vue
+++ frontend/src/views/app/publishes/detail-post.vue
@@ -6,6 +6,7 @@ import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { ChevronUp, ChevronDown } from 'lucide-vue-next'
 import InputQuillEditor from '@/components/input-quill-editor.vue'
+import { $dayjs } from '@/lib/dayjs'
 
 const $route = useRoute()
 const $notify = useNotify()
@@ -33,19 +34,23 @@ onMounted(async () => {
 
 <template>
   <div class="flex flex-col items-center justify-center pl-2 pt-5">
-    <div>
-      <span>joelbrs</span>
-      <span>{{ post.created_at }}</span>
-    </div>
-    <div class="flex items-start justify-center">
-      <div class="flex py-2 flex-col items-center">
-        <ChevronUp class="w-4 h-4 text-[#6e7681]" />
-        <span class="text-blue-600 text-xs font-bold">{{ post.tabcoins }}</span>
-        <ChevronDown class="w-4 h-4 text-[#6e7681]" />
+    <div class="flex gap-2 text-xs md:text-sm self-start md:self-center ml-7">
+      <div class="rounded-sm bg-[rgba(56,139,253,0.1)] px-2 text-center">
+        <span class="text-blue-600">joelbrs</span>
       </div>
-      <div class="w-full lg:py-2 md:w-[45%]">
-        <h1 class="text-3xl font-medium pl-3">{{ post.title }}</h1>
-
+      <div>
+        <span class="text-[#848d97]">{{ $dayjs(post.created_at).fromNow() }} atrás</span>
+      </div>
+    </div>
+    <div class="flex justify-center">
+      <div class="flex flex-col items-center gap-4 md:gap-2 md:h-[50vh] lg:h-72 h-[55vh]">
+        <ChevronUp class="w-5 h-5 text-[#6e7681]" />
+        <span class="text-blue-600 text-xs font-bold">{{ post.tabcoins }}</span>
+        <ChevronDown class="w-5 h-5 text-[#6e7681]" />
+        <div class="outline-dashed text-[#6e7681] outline-1 flex-grow"></div>
+      </div>
+      <div class="w-full md:py-2 md:w-[45%] flex flex-col">
+        <h1 class="text-3xl md:text-4xl font-medium pl-3">{{ post.title }}</h1>
         <div class="flex flex-col gap-2">
           <InputQuillEditor class="border-none" :key="key" v-model="post.description" disabled />
         </div>
